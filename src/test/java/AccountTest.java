8f81f719f191da1ce20fd69291f8a9d9645cca6b
Yohan <yohanlamerand@gmail.com>
2020-10-04 22:38:34 +0200

    refactoring

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,17 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class AccountTest {
 
+    private Account account;
+
+    @BeforeEach
+    void setup(){
+        account = new Account();
+    }
+
     @Test
     void an_new_account_created_have_0_money(){
-        Account account = new Account();
         assertThat(account.getMoney()).isEqualTo(new Money(0));
     }
 
     @Test
     void should_deposits_10_on_my_account() {
-        Account account = new Account();
         assertThat(
                 account.deposits(new Money(10)))
                 .isEqualTo(new Money(10)
@@ -20,7 +26,6 @@ public class AccountTest {
 
     @Test
     void should_deposits_50_on_my_account() {
-        Account account = new Account();
         assertThat(
                 account.deposits(new Money(50)))
                 .isEqualTo(new Money(50)
@@ -29,7 +34,7 @@ public class AccountTest {
 
     @Test
     void should_withdraw_10_on_my_account_which_there_is_10() {
-        Account account = new Account(new Money(10));
+        account = new Account(new Money(10));
         assertThat(
                 account.withdraw(new Money(10)))
                 .isEqualTo(new Money(0));
@@ -37,7 +42,7 @@ public class AccountTest {
 
     @Test
     void should_withdraw_20_on_my_account_which_there_is_50_and_the_rest_is_30() {
-        Account account = new Account(new Money(50));
+        account = new Account(new Money(50));
         assertThat(
                 account.withdraw(new Money(20)))
                 .isEqualTo(new Money(30));
